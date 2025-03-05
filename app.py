da47cb67733cbe720e3bc209da564d0ffc24f188
root <root@Pawan.>
2025-03-05 08:05:51 +0000

    Fix: Use dynamic port for Render deployment

--- app.py
+++ app.py
@@ -63,5 +63,9 @@ def get_threats():
     threats = list(threats_collection.find({}, {"_id": 0}))
     return jsonify(threats)
 
+
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Use Render-assigned port
+    app.run(debug=True, host="0.0.0.0", port=port)
