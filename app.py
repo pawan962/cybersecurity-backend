a8550c1f7b6eb3b4f81f1e273cd5c2ae68d693f7
root <root@Pawan.>
2025-03-07 03:46:10 +0000

    Fixed incorrect port issue

--- app.py
+++ app.py
@@ -83,5 +83,6 @@ def get_threats():
     return jsonify(threats)
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
